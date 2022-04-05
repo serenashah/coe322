e933edbd0b051f15c50c56677eb267c2c38edafe
Serena Shah <serena.shah@utexas.edu>
2022-04-05 14:21:41 -0500

    app.py

--- homework05/app.py
+++ homework05/app.py
@@ -5,7 +5,6 @@ from flask import Flask, request
 app = Flask(__name__)
 
 ml_data = {}
-
 @app.route('/data', methods=['POST'])
 def download_data(): 
     '''
@@ -18,9 +17,15 @@ def download_data():
     '''
     global ml_data
 
-    with open('ML_Data_sample.json', 'r') as f:
+    with open('ML_Data_Sample.json', 'r') as f:
                ml_data = json.load(f)
-    return "Data has been loaded.\n"
+    global rd
+    rd = redis.Redis(host='172.17.0.13', port = 6379, db = 0)
+    for d in ml_data['meteorite_landings']:
+            rd.set(d['id'],json.dumps(d))
+
+    
+    return "Data has been loaded!\n"
 
 @app.route('/data', methods=['GET'])
 def load_redis():
@@ -31,12 +36,11 @@ def load_redis():
     Returns:
         A JSON list of the information.
     '''   
-    rd = redis.Redis(host='172.17.0.13', port = 6379, db =0)
-    list_of_ml = []
-    for d in ml_data['meteorite_landings']:
-        list_of_ml.append(d)
-    return (json.dumps(list_of_ml), '\n')
+    list_of_data = []
+    for x in ml_data['meteorite_landings']:
+        list_of_data.append(json.loads(rd.get(x['id'])))
+    return (json.dumps(list_of_data, indent=1) + '\n')
 
 if __name__ == "__main__":
-    d = ml_data
     app.run(debug=True, host = '0.0.0.0')
+    print(load_redis())
